f2cd6bbd5d5abcb54b150de2d8e9025927101b83
Stateless-X <purin.buriwong@gmail.com>
2024-09-13 16:14:10 +0700

    [UPDATED] fixed total donated amount incorrect after payment

--- challenge-react/src/index.tsx
+++ challenge-react/src/index.tsx
@@ -22,7 +22,8 @@ const store = createStore(function (state:DonationState, action:DonationAction) 
   switch (action.type) {
     case 'UPDATE_TOTAL_DONATE':
       return Object.assign({}, _state, {
-        donate: _state.donate + action.amount,
+        ...state,
+        donate: action.amount,
       });
     case 'UPDATE_MESSAGE':
       return Object.assign({}, _state, {
