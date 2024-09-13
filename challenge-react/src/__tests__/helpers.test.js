08886209a19749214d02309b286cba669afdb24c
Stateless-X <purin.buriwong@gmail.com>
2024-09-13 17:11:06 +0700

    [UPDATED] Edited and Correct the test case
    - Have issue with lib unable to run jest. no time to fix. quickly added my cases for my helper.

--- challenge-react/src/__tests__/helpers.test.js
+++ challenge-react/src/__tests__/helpers.test.js
@@ -1,7 +1,17 @@
-import { summaryDonations } from '../helpers';
+import { summaryDonations, formatCurrency } from '../helpers';
 
 describe('helpers', function () {
   test('`summaryDonations` should calculate donations correctly', function () {
-    expect(summaryDonations([1, 2, 3, 4])).toEqual(11);
+    expect(summaryDonations([1, 2, 3, 4])).toEqual(10);
+  });
+
+  test('correctly formats amount with comma', () => {
+    const result = formatCurrency('1000');
+    expect(result).toBe('1,000');
+  });
+
+  test('correctly return empty string', () => {
+    const result = formatCurrency('');
+    expect(result).toBe('');
   });
 });
